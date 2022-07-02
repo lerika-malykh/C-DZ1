923a5429fbf306135712962caa61e0bec1c6081e
lerika_malykh <lerusik1808@yandex.ru>
2022-07-02 15:46:38 +0300

    Решение задачи 21

--- Seminar_3/DZ/Program.cs
+++ Seminar_3/DZ/Program.cs
@@ -1,9 +1,15 @@
-﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-3 -> 1, 8, 27
-5 -> 1, 8, 27, 64, 125*/
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
-int N = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= N; i ++)
-{
-    Console.WriteLine("Куб числа " + i + " = " + Math.Pow(i, 3));
-}
+Console.WriteLine("Введиты координаты 1-й точки: ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+int Y1 = Convert.ToInt32(Console.ReadLine());
+int Z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введиты координаты 2-й точки: ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+int Y2 = Convert.ToInt32(Console.ReadLine());
+int Z2 = Convert.ToInt32(Console.ReadLine());
+double distance = 0;
+distance = Math.Round(Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2)), 2);
+Console.Write("Расстояние между точками = " + distance);
