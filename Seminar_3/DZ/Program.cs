09077613f73dd99fe0bc32be49895638bd2cafa4
lerika_malykh <lerusik1808@yandex.ru>
2022-07-02 16:13:32 +0300

    Решение задач

--- Seminar_3/DZ/Program.cs
+++ Seminar_3/DZ/Program.cs
@@ -1,6 +1,19 @@
-﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+﻿/*19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+
+Console.WriteLine("Введите 5ти-значное число: ");
+string number = Console.ReadLine();
+bool palindrom(string number)
+{
+    return number[0]==number[4] && number[1]==number[3];
+}
+Console.WriteLine(palindrom(number));*/
+
+/* 21.Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
-A (7,-5, 0); B (1,-1,9) -> 11.53*/
+A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.WriteLine("Введиты координаты 1-й точки: ");
 int X1 = Convert.ToInt32(Console.ReadLine());
@@ -12,4 +25,14 @@ int Y2 = Convert.ToInt32(Console.ReadLine());
 int Z2 = Convert.ToInt32(Console.ReadLine());
 double distance = 0;
 distance = Math.Round(Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2)), 2);
-Console.Write("Расстояние между точками = " + distance);
+Console.Write("Расстояние между точками = " + distance);*/
+
+/*23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i ++)
+{
+    Console.WriteLine("Куб числа " + i + " = " + Math.Pow(i, 3));
+}
