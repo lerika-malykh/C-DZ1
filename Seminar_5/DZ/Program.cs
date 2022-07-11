a48773a79518acabc0559372c3329b3b38ad799a
lerika_malykh <lerusik1808@yandex.ru>
2022-07-11 17:54:14 +0300

    Задача 38

--- Seminar_5/DZ/Program.cs
+++ Seminar_5/DZ/Program.cs
@@ -21,7 +21,7 @@ Console.WriteLine($"количество четных элементов: {sum}"
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19; [-4, -6, 89, 6] -> 0
 
-int[] array = new int [new Random().Next(1, 11)];
+/*int[] array = new int [new Random().Next(1, 11)];
 int size = array.Length;
 int oddIndex = 0;
 int sum = 0;
@@ -36,4 +36,24 @@ for (int i = 0; i < size; i++)
     }
 }
 Console.WriteLine($"Исходный массив: [{String.Join(",", array)}]");
-Console.WriteLine($"Сумма элементов на нечетных местах: {sum}");
+Console.WriteLine($"Сумма элементов на нечетных местах: {sum}");*/
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+double[] array = new double[10];
+int size = array.Length;
+double result = 0;
+double max = array[0];
+double min = array[0];
+for (int i = 0; i < size; i++)
+{
+    array[i] = Math.Round((new Random().Next(-100, 100) * 0.01), 3);
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+Console.WriteLine($"Исходный массив: [{String.Join("; ", array)}]");
+Console.WriteLine($"max = {max}");
+Console.WriteLine($"min = {min}");
+result = Math.Round(Math.Abs(max - min), 3);
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {result}");
