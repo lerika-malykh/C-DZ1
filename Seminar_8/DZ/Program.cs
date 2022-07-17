d5456728f21907bc206d3a3fbcfc950d858286c3
lerika_malykh <lerusik1808@yandex.ru>
2022-07-17 17:13:27 +0300

    задача 58

--- Seminar_8/DZ/Program.cs
+++ Seminar_8/DZ/Program.cs
@@ -57,7 +57,7 @@ PrintArray(array);
 */
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-Console.Write("Введите количество строк массива: ");
+/*Console.Write("Введите количество строк массива: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
@@ -108,3 +108,63 @@ for (int i = 0; i < m; i++)
     }
 }
 Console.Write($"Строка c индексом {row} имеет минимальную сумму = {minSum}");
+*/
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] GetArray(int m, int n)
+{
+    int[,] resultArray = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            resultArray[i, j] = new Random().Next(11);
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк 1-й матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов 1-й матрицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк 2-й матрицы: ");
+int p = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов 2-й матрицы: ");
+int q = Convert.ToInt32(Console.ReadLine());
+int[,] oneMatrix = GetArray(m, n);
+Console.WriteLine("Матрица 1:");
+PrintArray(oneMatrix);
+Console.WriteLine();
+int[,] twoMatrix = GetArray(p, q);
+Console.WriteLine("Матрица 2:");
+PrintArray(twoMatrix);
+int [,] resultMatrix = new int [m,q];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < q; j++)
+    {
+        int sum =0;
+        for (int z = 0; z < p; z++)
+        {
+            sum += oneMatrix[i,z]*twoMatrix[z,j];
+        }
+        resultMatrix[i,j] = sum;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Произведение матриц:");
+PrintArray(resultMatrix);
