b6db4ddc3e66fbf2decac7e33d2ea097d4a879b1
lerika_malykh <lerusik1808@yandex.ru>
2022-07-17 14:58:34 +0300

    Задача 52

--- Seminar_7/DZ/Program.cs
+++ Seminar_7/DZ/Program.cs
@@ -44,7 +44,7 @@ PrintArray(matrix);
 8 4 2 4
 17 -> такого числа в массиве нет*/
 
-Console.WriteLine("Введите количество строк массива:");
+/*Console.WriteLine("Введите количество строк массива:");
 int m = Int32.Parse(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов массива:");
 int n = Int32.Parse(Console.ReadLine());
@@ -84,3 +84,36 @@ if(indexRows > m || indexColumns > n)
     return;
 }
 Console.WriteLine($"Искомый элемент {matrix[indexRows, indexColumns]}");
+*/
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+/*Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+Console.WriteLine("Введите количество строк массива:");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива:");
+int columns = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int [rows,columns];
+Console.WriteLine("Массив:");
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(11);
+        Console.Write(matrix[i,j] + "\t");        
+    }
+    Console.WriteLine();
+}
+double sum = 0; //сумм элементов
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i,j];
+    }
+    Console.WriteLine($"Среднее арифметическое элементов {j}-го столбца =  {sum/columns}"); 
+}
