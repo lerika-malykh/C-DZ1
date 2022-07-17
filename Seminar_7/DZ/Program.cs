607a6701e3aa9b33bb131d4c1f7a615684c4083d
lerika_malykh <lerusik1808@yandex.ru>
2022-07-17 14:22:57 +0300

    Задача 47

---
+++ Seminar_7/DZ/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+/*m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+Console.WriteLine("Введите количество строк массива:");
+int m = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива:");
+int n = Int32.Parse(Console.ReadLine());
+double[,] matrix = new double[m, n];
+void GetArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble()*10,2);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t"); 
+        }
+    Console.WriteLine();
+    }
+}
+Console.WriteLine("Массив вещественных чисел готов:");
+GetArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
